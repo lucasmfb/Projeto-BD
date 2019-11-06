1cc8b4f5d70407b4a0816811c24fff5a51d013e5
lucasmfb <lucasmfb14@gmail.com>
2019-11-05 22:40:55 -0300

    atualizando

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,143 +1,195 @@
+-- Esquema padrão para o projeto de Banco de Dados 1 2019.2
+-- Grupo 06
+
 CREATE TABLE  cliente (
-        cpf_cliente CHAR(11) NOT NULL, 
-	nome VARCHAR2(50) NOT NULL, 
+        cpf_cliente VARCHAR2(14), 
+	nome VARCHAR2(100) NOT NULL, 
 	email VARCHAR2(50) NOT NULL, 
 	pontos_crm INT NOT NULL, 
-	rua VARCHAR(50)NOT NULL,
+	rua VARCHAR2(100) NOT NULL,
         numero INT,
-        cidade VARCHAR(30) NOT NULL,
-        bairro VARCHAR(50) NOT NULL,
-        estado VARCHAR (20) NOT NULL,
+        cidade VARCHAR2(50) NOT NULL,
+        bairro VARCHAR2(50) NOT NULL,
+        estado VARCHAR2(50) NOT NULL,
+        
+        CONSTRAINT pk_cliente
 	PRIMARY KEY (cpf_cliente)
    );
 
 CREATE TABLE telefone_cliente(
-	cpf_cliente VARCHAR (20),
-    	numero_tel_cliente VARCHAR(20),
-	PRIMARY KEY (cpf_cliente)
+        cpf_cliente VARCHAR2 (14),
+        tel_cliente VARCHAR2(14),
+        
+        CONSTRAINT fk_telefone_cliente
+        FOREIGN KEY (cpf_cliente) REFERENCES cliente(cpf_cliente),
+        
+        CONSTRAINT pk_telefone_cliente
+        PRIMARY KEY (cpf_cliente, tel_cliente)
+        
 );
 
 CREATE TABLE funcionario(
-        matricula_funcionario VARCHAR(20) NOT NULL,
-        cpf_funcionario CHAR(11) NOT NULL,
-        identidade CHAR(14) NOT NULL,
-        nome VARCHAR(50) NOT NULL,
-        endereco_funcionario VARCHAR(50) NOT NULL,
+        matricula_funcionario VARCHAR2(20),
+        cpf_funcionario VARCHAR2(14) NOT NULL,
+        identidade VARCHAR2(14) NOT NULL,
+        nome VARCHAR2(100) NOT NULL,
+        endereco VARCHAR2(50) NOT NULL,
         salario FLOAT NOT NULL,
-        funcao VARCHAR(20) NOT NULL,
-        matricula_supervisor VARCHAR(20),
+        funcao VARCHAR2(20) NOT NULL,
+        matricula_supervisor VARCHAR2(20),
+        
+        CONSTRAINT pk_matricula_funcionario
         PRIMARY KEY (matricula_funcionario),
+        
+        CONSTRAINT fk_matricula_supervisor
         FOREIGN KEY (matricula_supervisor) REFERENCES funcionario(matricula_funcionario)
 
 );
 
 CREATE TABLE telefone_funcionario(
-	matricula_funcionario VARCHAR (20),
-	numero_tel_funcionario VARCHAR(20),
-	PRIMARY KEY (matricula_funcionario)
+        matricula_funcionario VARCHAR2 (20),
+        tel_funcionario VARCHAR2(20),
+        
+        CONSTRAINT fk_telefone_funcionarrio
+        FOREIGN KEY (matricula_funcionario) REFERENCES funcionario(matricula_funcionario),
+        
+        CONSTRAINT pk_telefone_funcionario
+        PRIMARY KEY (matricula_funcionario, tel_funcionario)
 );
 
 CREATE TABLE dependente(
-        cpf_dependente CHAR(11) NOT NULL,
-        data_nascimento DATE NOT NULL,
-        nome_dependente VARCHAR(50) NOT NULL,
-        matricula_dependente VARCHAR(20),
-        PRIMARY KEY (cpf_dependente),
-        FOREIGN KEY (matricula_dependente) REFERENCES funcionario(matricula_funcionario)
- );
+        cpf VARCHAR2(14),
+        data_nasc DATE NOT NULL,
+        nome VARCHAR2(100) NOT NULL,
+        matricula_funcionario VARCHAR2(14) NOT NULL,
+    
+        CONSTRAINT fk_dependente_funcionario
+        FOREIGN KEY (matricula_funcionario) REFERENCES  funcionario(matricula_funcionario),
+	
+	CONSTRAINT pk_dependente
+	PRIMARY KEY (cpf, matricula_funcionario)	
+);
 
 CREATE TABLE equipamento(
-        id_equipamento NUMBER NOT NULL,
-        descricao VARCHAR(100) NOT NULL,
+        id_equipamento INT,
+        descricao VARCHAR2(140) NOT NULL,
+        
+        CONSTRAINT pk_id_equipamento
         PRIMARY KEY (id_equipamento)
 );
 
 CREATE TABLE nota_fiscal(
-        num_nota_fiscal NUMBER NOT NULL,
-        cnpj CHAR(12) NOT NULL,
+        num_nota NUMBER, 
+        cnpj CHAR(14) NOT NULL,
         quantidade INT NOT NULL,
         data DATE NOT NULL,
         valor_por_item FLOAT NOT NULL,
-        PRIMARY KEY (num_nota_fiscal)
+        
+        CONSTRAINT pk_num_nota
+        PRIMARY KEY (num_nota)
 );
 
 CREATE TABLE solicitacao(
-        id_solicitacao NUMBER NOT NULL,
+        id_solicitacao INT,
         data_solicitacao DATE NOT NULL,
         data_prev_entrega DATE NOT NULL,
         data_entrega DATE NOT NULL,
         valor_compra FLOAT NOT NULL,
         prazo_pagamento INT NOT NULL,
+        
+        CONSTRAINT pk_id_solicitacao
         PRIMARY KEY (id_solicitacao)
 );
 
 CREATE TABLE item(
-        id_item NUMBER NOT NULL,
+        id_item INT,
         quantidade INT NOT NULL,
         preco FLOAT NOT NULL,
         desconto FLOAT,
+        
+        CONSTRAINT pk_id_item
         PRIMARY KEY (id_item)
 );
 
-CREATE TABLE  ordem_de_compra(
-        num_nota_fiscal NUMBER NOT NULL, 
+CREATE TABLE ordem_de_compra(
+        num_nota_fiscal INT, 
 	data_hora DATE NOT NULL, 
+        
+        CONSTRAINT pk_num_nota_fiscal
 	PRIMARY KEY (num_nota_fiscal)
 );
 
-CREATE TABLE  produto(
-        id_produto NUMBER NOT NULL,
+CREATE TABLE produto(
+        id_produto INT,
 	nome VARCHAR2(30) NOT NULL,
 	descricao VARCHAR2(40) NOT NULL,
 	margem_lucro FLOAT(126) NOT NULL, 
+        
+        CONSTRAINT pk_id_produto
 	PRIMARY KEY (id_produto)
 );
 
-CREATE TABLE  filial(
-        id_filial INT NOT NULL,
+CREATE TABLE filial(
+        id_filial INT,
         nome VARCHAR(50) NOT NULL,
         endereco  VARCHAR(100) NOT NULL, 
         telefone VARCHAR(20) NOT NULL,
+        
+        CONSTRAINT pk_id_filial
         PRIMARY KEY(id_filial)
    
 );
 
 CREATE TABLE marca(
-        id_marca VARCHAR (12) NOT NULL,
+        id_marca INT,
         nome_marca VARCHAR (50) NOT NULL,
+        
+        CONSTRAINT pk_id_marca
         PRIMARY KEY (id_marca)
 );
 
 CREATE TABLE categoria(
-        id_categoria VARCHAR (12) NOT NULL,
+        id_categoria INT,
         nome_categoria VARCHAR (50) NOT NULL,
+        
+        CONSTRAINT pk_id_categoria
         PRIMARY KEY (id_categoria)
 );
 
-CREATE TABLE caixa (
-        num_caixa INT NOT NULL,
+CREATE TABLE caixa(
+        num_caixa INT,
         cod_id_caixa INT NOT NULL,
+        
+        CONSTRAINT pk_num_caixa
         PRIMARY KEY (num_caixa)
 );
 
-CREATE TABLE fornecedor (
-        cnpj_fornecedor CHAR(14) NOT NULL,
+CREATE TABLE fornecedor(
+        cnpj_fornecedor VARCHAR2(14),
         nome VARCHAR(30) NOT NULL,
         endereco VARCHAR(100) NOT NULL,
         email VARCHAR(50) NOT NULL,
         site VARCHAR(30) NOT NULL,
+        
+        CONSTRAINT pk_cnpj_fornecedor
         PRIMARY KEY (cnpj_fornecedor)
 );
 
 CREATE TABLE telefone_fornecedor(
-        cnpj_fornecedor VARCHAR(20),
-        numero_tel_fornecedor VARCHAR(20),
-        PRIMARY KEY (cnpj_fornecedor)
+	telefone VARCHAR2(14),
+	cnpj_fornecedor VARCHAR2(14),
+
+        CONSTRAINT fk_telefone_fornecedor
+        FOREIGN KEY (cnpj_fornecedor) 
+        REFERENCES  fornecedor(cnpj_fornecedor),
+
+	CONSTRAINT pk_telefone_fornecedor
+	PRIMARY KEY (cnpj_fornecedor, telefone)
 );
 
 CREATE TABLE realiza_reclamacao(
-        cpf_cliente CHAR(11) NOT NULL,
-        id_filial INT NOT NULL,
+        cpf_cliente VARCHAR(11),
+        id_filial INT,
 	data_e_hora VARCHAR(50),
 	descricao VARCHAR(60),
        	FOREIGN KEY (cpf_cliente) REFERENCES cliente(cpf_cliente),
@@ -146,8 +198,8 @@ CREATE TABLE realiza_reclamacao(
 );
 
 CREATE TABLE realiza_manutencao(
-        id_equipamento NUMBER NOT NULL,
-        matricula_funcionario VARCHAR(20) NOT NULL,
+        id_equipamento NUMBER,
+        matricula_funcionario VARCHAR(20),
 	data_e_hora VARCHAR(50),
 	descricao VARCHAR(60),
        	FOREIGN KEY (id_equipamento) REFERENCES equipamento(id_equipamento),
